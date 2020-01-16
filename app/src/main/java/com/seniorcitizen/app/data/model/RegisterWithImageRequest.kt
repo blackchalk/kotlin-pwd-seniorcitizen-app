e6d1b6f166f29b6087320fc0ab98e1597a1bbfdd
Alvin L. Raygon <alvinraygon@yahoo.com>
2020-01-16 20:15:09 +0800

    Added model class for registration request with profile image

---
+++ app/src/main/java/com/seniorcitizen/app/data/model/RegisterWithImageRequest.kt
@@ -0,0 +1,42 @@
+package com.seniorcitizen.app.data.model
+
+import com.squareup.moshi.Json
+
+data class RegisterWithImageRequest(
+
+	@Json(name="Address")
+	var Address: String? = null,
+
+	@Json(name="Username")
+	var Username: String? = null,
+
+	@Json(name="FirstName")
+	var FirstName: String? = null,
+
+	@Json(name="Sex")
+	var Sex: String? = null,
+
+	@Json(name="IsPWD")
+	var IsPWD: Boolean? = null,
+
+	@Json(name="Birthday")
+	var Birthday: String? = null,
+
+	@Json(name="LastName")
+	var LastName: String? = null,
+
+	@Json(name="MiddleName")
+	var MiddleName: String? = null,
+
+	@Json(name="IsSenior")
+	var IsSenior: Boolean? = null,
+
+	@Json(name="IDNumber")
+	var IDNumber: String? = "0",
+
+	@Json(name="Password")
+	var Password: String? = null,
+
+	@Json(name="SeniorImage")
+	var SeniorImage: String? = null
+)
